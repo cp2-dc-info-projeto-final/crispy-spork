9bcf0620a4ea2006684ac9f8d9a0a437bb1a8d76
Ygor Canalli <ygor.canalli@gmail.com>
2021-02-15 03:24:48 -0300

    run create schema

--- src/index.php
+++ src/index.php
@@ -8,6 +8,20 @@
     require_once "connection.php";
 
     $conn = get_connection();
+
+    $commands = file_get_contents("../create-schema.sql");   
+    $result = mysqli_multi_query($conn, $query);
+
+    if ($result) {
+        do {
+            // grab the result of the next query
+            if (($result = mysqli_store_result($mysqli)) === false && mysqli_error($mysqli) != '') {
+                echo "Query failed: " . mysqli_error($mysqli);
+            }
+        } while (mysqli_more_results($mysqli) && mysqli_next_result($mysqli)); // while there are more results
+    } else {
+        echo "First query failed..." . mysqli_error($mysqli);
+    }
     echo '<p>Hello World</p>';
     for ($i = 0; $i < 10 ; $i++)
     {
